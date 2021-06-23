d13f6a4218082482e37e25b247d110c3b3995518
Admin <Admin@DESKTOP-63EL9EP>
2021-06-23 16:33:11 +0900

    마지막

---
+++ firstProject/src/firstProject/StringExample.java
@@ -0,0 +1,10 @@
+package firstProject;
+
+public class StringExample {
+public static void main(String[] args) {
+	String name = "Hongkildong";
+	int age = 16;
+	System.out.println("이름은 "+ name + " 이고 나이는 " + age + " 입니다.");
+	
+}
+}
